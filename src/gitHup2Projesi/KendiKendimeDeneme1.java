5fa5ccb825dd2bf3c5a00a30f35d11dc9de8acd4
aselyacan <iskender1982@gmail.com>
2020-06-22 12:33:14 +0200

    ikinci deneme  eclipsden olan ilave

--- src/gitHup2Projesi/KendiKendimeDeneme1.java
+++ src/gitHup2Projesi/KendiKendimeDeneme1.java
@@ -4,7 +4,8 @@ public class KendiKendimeDeneme1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("ismail beyle ilk deneme ");
+      System.out.println("ismail beyle ilk deneme ");
+      System.out.println("ikinci deneme ");
 	}
 
 }
